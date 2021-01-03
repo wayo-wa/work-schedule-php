0b3664fcdf20d59ffe8df1c341d82f1b8b3e8171
w.a <yumab187jp-wk@yahoo.co.jp>
2021-01-03 16:09:05 +0900

    make it better

--- auth.php
+++ auth.php
@@ -21,10 +21,10 @@ if(!empty($_SESSION['login_date'])) {
     }
 }else{
     debug('未ログインユーザー');
-    if(basename($_SERVER['PHP_SELF']) === 'mypage.php' || basename($_SERVER['PHP_SELF']) === 'stamping.php'){
-        header("Location:mypage.php");
-        // header("Location:index.php");
-        exit();
-    }
+    header("Location:mypage.php");
+    // if(basename($_SERVER['PHP_SELF']) === 'mypage.php' || basename($_SERVER['PHP_SELF']) === 'stamping.php'){
+    //     header("Location:index.php");
+    //     exit();
+    // }
 }
 
